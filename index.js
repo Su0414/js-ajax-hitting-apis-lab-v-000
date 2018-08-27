72701fb213c39275d566230ae16637395be332e0
Suchitra Naik <susanaik@gmail.com>
2018-08-27 21:27:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' 
+  + r.name 
+  + ' - <a href="#" data-repo="' 
+  + r.name 
+  + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
 function getRepositories() {
   const name = document.getElementById("username").value
+  const url = rootURL + "/users/" + name + "/repos"
+  
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/:username/repos')
+  req.open("GET", url)
   req.send()
 }
 

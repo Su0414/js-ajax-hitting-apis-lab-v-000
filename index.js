299aeca18edb5929bac527a2d28f6d6f36d104ae
Suchitra Naik <susanaik@gmail.com>
2018-08-27 21:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name
-  + ' - <a href="#" data-repo="' + r.name
+  + ' - <a href="#" data-repo="' + r.html_url
   + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
